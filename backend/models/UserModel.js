6d823234b5d8174772756a94b9f3b4614ab0604f
arushi <arushimadhesiya309@gmail.com>
2024-04-21 12:46:45 +0530

    pw bcrypted

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,4 +1,5 @@
 const mongoose=require('mongoose');
+const bcryptjs=require('bcryptjs');
 const UserModel=new mongoose.Schema({
     name:{
         type:String,
@@ -6,19 +7,32 @@ const UserModel=new mongoose.Schema({
     },
     email:{
         type:String,
-        required:true
+        required:true,
+        unique: true
     },// this message is a part of which chat
     password:{
         type:String,
         required:true
     },
-    photo:{
+    pic:{
         type:String,
-        required:true,
+        
         default:"https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"
     }
 },
 {
     timestamps:true
+})
+UserModel.methods.pwMatches=async function(enteredPw){
+    return await bcryptjs.compare(enteredPw,this.password);
+}
+UserModel.pre('save',async function (next) {
+    if(!this.isModified){
+        next()
+    }
+    else{
+        const salt=await bcryptjs.genSalt(10);
+        this.password=await bcryptjs.hash(this.password,salt);
+    }
 })
 module.exports=mongoose.model("UserModel",UserModel);
