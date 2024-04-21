8dd2994d577ef05a7b39fe83410cbb76925db164
arushi <arushimadhesiya309@gmail.com>
2024-04-21 12:45:20 +0530

    login done

--- backend/controller/UserController.js
+++ backend/controller/UserController.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
 const UserModel = require("../models/UserModel");
+//------------signup
 const registerUser = asyncHandler(async (req, res) => {
-  const { name, email, pw, pic } = req.body;
+  const { name, email, password, pic } = req.body;
   console.log("name:", name);
   console.log("email:", email);
-  console.log("pw:", pw);
-  if (!name || !email || !pw) {
+  console.log("pw:", password);
+  if (!name || !email || !password) {
     console.warn("fill all ma'am");
     throw new Error("fill all maaaaaaaam");
   }
@@ -15,7 +16,7 @@ const registerUser = asyncHandler(async (req, res) => {
     console.warn("already present");
     throw new Error("this email already in use");
   }
-  const newUser = await UserModel.create({ name, email, pw, pic });
+  const newUser = await UserModel.create({ name, email, password, pic });
   if (newUser) {
     res.status(201).json({
       _id: newUser._id,
@@ -28,5 +29,26 @@ const registerUser = asyncHandler(async (req, res) => {
     res.send("user couldnt be registered");
   }
 });
-
-module.exports = { registerUser };
+//------------Login
+const LoginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  console.log("email:", email);
+  console.log("pw:", password);
+  // email and its password matches
+  const userExists = await UserModel.findOne({ email });
+  if (userExists) {
+    const check = userExists.pwMatches(password);
+    if ( check) {
+      res.status(201).json({
+        _id: userExists._id,
+        name: userExists.name,
+        email: userExists.email,
+        pic: userExists.pic,
+      });
+    }
+  } else {
+    res.status(400);
+    res.send("user not found");
+  }
+});
+module.exports = { registerUser, LoginUser };
